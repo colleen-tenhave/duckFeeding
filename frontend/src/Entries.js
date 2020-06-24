a381bfebab1f42fcd45cacb1e6b7eead15921d1d
unknown <colleentenhave@gmail.com>
2020-06-23 21:21:34 -0700

    create entries component, render list of all entries

---
+++ frontend/src/Entries.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const feedingDataEntry = "http://localhost:9000/feedingDataEntry";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { submissions: []};
+  }
+
+  fetchEntries() {
+    fetch("http://localhost:9000/feedingDataEntry")
+    .then(res => res.text())
+    .then(res => this.setState({ submissions: JSON.parse(res)}));
+  }
+
+componentWillMount(){
+    this.fetchEntries();
+ }
+
+  render() {
+        console.log(typeof this.state.submissions);
+        const data = this.state.submissions;
+        return (
+          <div>
+            <p>
+              Submissions From Fellow Duck Lovers: 
+            </p>
+            {data.map(function(entry, idx){
+              return (<div key={idx}>Feeding Time: {entry.feedingTime}, Feeding Location: {entry.feedingLocation},
+              Food Type: {entry.foodType}, Number of Ducks Fed: {entry.numberOfDucks}, Amount of Food: {entry.quantityOfFood}</div>)
+            })}
+          </div>
+        );
+  }
+}
+export default App;
